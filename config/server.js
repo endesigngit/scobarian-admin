0a4e22d45f8d00fa91f1998b454c6e8dc39fc92a
root <root@mikhailo23-vps-1>
2024-10-03 16:58:55 +0300

    schema update

--- config/server.js
+++ config/server.js
@@ -1,6 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '127.0.0.1'),
+  //port: env.int('PORT', 1337),
+  port: 1338,
+  url: 'http://admin.skobarian.ru/', 
   app: {
     keys: env.array('APP_KEYS'),
   },
