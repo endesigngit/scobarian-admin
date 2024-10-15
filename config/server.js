e2e67b620ef16b855ded18ceacedb320ee54e4a2
chv-roman <chv-roman@mail.ru>
2024-10-15 19:06:00 +0300

    FIx

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '127.0.0.1'),
   // port: env.int('PORT', 1337),
   port: 1338,
-  url: 'http://admin.skobarian.ru/', 
+  url: 'https://admin.skobarian.ru/', 
   app: {
     keys: env.array('APP_KEYS'),
   },
