d93a8a63d0b17f1d95e001084eaf73cbc55bec4d
chv-roman <chv-roman@mail.ru>
2024-10-24 14:43:57 +0300

    -fix: Исправлен вид формы обратной связи в письме для почты

--- src/api/feedback/content-types/feedback/lifecycles.js
+++ src/api/feedback/content-types/feedback/lifecycles.js
@@ -1,11 +1,51 @@
 module.exports = {
   afterCreate(event) {
     const { result, params } = event;
+
+    const email = params.data.userEmail
+    const name = params.data.userName
+    const phone = params.data.userPhone
+    const text = params.data.userText
+    const nameHtml = name ? `
+    <div>
+      <span>Имя:</span>
+      <span>${name}</span>
+    </div>
+  ` : ""
+  const emailHtml = (email && email != "notmail@no.ru") ? `
+    <div>
+      <span>Email:</span>
+      <span>${email}</span>
+    </div>
+  ` : ""
+  const phoneHtml = phone ? `
+    <div>
+      <span>Телефон:</span>
+      <span>${phone}</span>
+    </div>
+  ` : ""
+  const textHtml = text ? `
+    <div>
+      <span>Компания:</span>
+      <span>${text}</span>
+    </div>
+  ` : ""
     
     strapi.plugins['email'].services['email'].send({
       to: ['chernyshov@endesign.ru', 'mikhailov@pskovlive.ru'],
       from: 'no-reply@skobarian.ru',
-      text: 'Получено новое сообщение! Подробнее на <a href="http://admin.skobarian.ru/">admin</a>',
+      subject: "Форма обратной связи",
+      html: `
+        <div>
+          <h3>У Вас новый запрос в форме обратной связи!</h3>
+          <hr>
+          ${nameHtml}
+          ${emailHtml}
+          ${phoneHtml}
+          ${textHtml}
+        </div>
+        `,
+      // text: 'Получено новое сообщение! Подробнее на <a href="http://admin.skobarian.ru/">admin</a>',
     });
   },
 };
