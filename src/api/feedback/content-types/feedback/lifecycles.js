5e3d9db86823f7b979b00e04b295763bec384b07
chv-roman <chv-roman@mail.ru>
2024-10-24 13:47:42 +0300

    -add: Добавдена почта

--- src/api/feedback/content-types/feedback/lifecycles.js
+++ src/api/feedback/content-types/feedback/lifecycles.js
@@ -3,7 +3,7 @@ module.exports = {
     const { result, params } = event;
     
     strapi.plugins['email'].services['email'].send({
-      to: 'chernyshov@endesign.ru',
+      to: ['chernyshov@endesign.ru', 'mikhailov@pskovlive.ru'],
       from: 'no-reply@skobarian.ru',
       text: 'Получено новое сообщение! Подробнее на <a href="http://admin.skobarian.ru/">admin</a>',
     });
